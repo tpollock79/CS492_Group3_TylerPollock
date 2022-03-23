da650638f2231c99064b00d640ea54b036f0303f
tpollock79 <101008353+tpollock79@users.noreply.github.com>
2022-03-23 15:41:36 -0500

    Update bookstore_checkpoint1.py

--- bookstore_checkpoint1.py
+++ bookstore_checkpoint1.py
@@ -4,7 +4,6 @@
 #March 13th, 2022
 import tkinter as tk
 from tkinter import ttk
-from functools import partial
 import sqlite3
 from tkinter import *
 from tkinter import messagebox
@@ -12,6 +11,7 @@ from PIL import ImageTk, Image
 
 root = tk.Tk()       #Create root window to prompt employee or customer login
 root.title('By The Books')
+
 window_width = 900
 window_height = 506
 screen_width = root.winfo_screenwidth()
@@ -29,7 +29,7 @@ panel.pack(side = "bottom", fill = "both", expand = "yes")
 def user_login(type, user, password):
     if type == 'cust':
         if user == "customerexample@gmail.com" and password == "1234567890":
-            show_store_customer()
+            show_store_customer(user, password)
         else:
             messagebox.showerror("Invalid", "Invalid Email or Phone Number")
     elif type == 'emp':
@@ -42,6 +42,7 @@ def user_login(type, user, password):
 def customer_click():
     login_window = tk.Tk()
     login_window.title('By The Books Customer Login')
+    login_window.iconbitmap('bythebooks.ico')
     window_width = 600
     window_height = 200
     screen_width = login_window.winfo_screenwidth()
@@ -65,10 +66,10 @@ def customer_click():
     passwordEntry.grid(row=1, column=1)
     loginButton = ttk.Button(login_window, text="Login", command=customer_login).grid(row=1, column=2)
 
-
 def employee_click():
     login_window = tk.Tk()
     login_window.title('By The Books Employee Login')
+    login_window.iconbitmap('bythebooks.ico')
     window_width = 600
     window_height = 200
     screen_width = login_window.winfo_screenwidth()
@@ -92,58 +93,46 @@ def employee_click():
     passwordEntry.grid(row=1, column=1)
     loginButton = ttk.Button(login_window, text="Login", command=emp_login).grid(row=1, column=2)
 
-
 user_button = ttk.Button(root, text='Customer login', command=customer_click).place(x = 400, y = 100)
 
 employee_button = ttk.Button(root, text='Employee login', command=employee_click).place(x = 400, y = 150)
 
-def show_store_customer():      #Function to create window with customer functions and establish connection to db's
+def show_store_customer(user, password):      #Function to create window with customer functions and establish connection to db's
+    cust = user
+    phone = password
     class DB:
         def __init__(self):
             self.conn = sqlite3.connect("bookstore.db")
             self.cur = self.conn.cursor()
             self.cur.execute(
-                "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, isbn TEXT, quantity TEXT)")
+                "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, isbn TEXT, quantity INTEGER)")
             self.conn.commit()
-
         def __del__(self):
             self.conn.close()
-
         def view(self):
             self.cur.execute("SELECT * FROM book")
             rows = self.cur.fetchall()
             return rows
-
-        def search(self, title="", author=""):
-            self.cur.execute("SELECT * FROM book WHERE title=? OR author=?", (title, author,))
+        def search(self, title, author, isbn):
+            self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR isbn=?", (title, author, isbn))
             rows = self.cur.fetchall()
             return rows
-
-        def purchase(self):
-            print('This is where the customer purchases their book')
-
+        def purchase(self, title, author, quantity):
+            self.cur.execute("UPDATE book SET quantity=quantity-? WHERE title=? AND author=?", (quantity, title, author,))
+            self.conn.commit()
     db = DB()
-
     class SDB:
         def __init__(self):
             self.conn = sqlite3.connect("sales.db")
             self.cur = self.conn.cursor()
             self.cur.execute(
-                "CREATE TABLE IF NOT EXISTS sales (orderID INTEGER PRIMARY KEY, customer TEXT, itemPurchased TEXT)")
+                "CREATE TABLE IF NOT EXISTS sales (orderID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, isbn TEXT, cust TEXT, phone TEXT)")
             self.conn.commit()
-
         def __del__(self):
             self.conn.close()
-
-        def view(self):
-            self.cur.execute("SELECT * FROM sales")
-            rows = self.cur.fetchall()
-            return rows
-
-        def search(self, customer="", itemPurchased=""):
-            self.cur.execute("SELECT * FROM book WHERE customer=? OR itemPurchased=?", (customer, itemPurchased,))
-            rows = self.cur.fetchall()
-            return rows
+        def purchase(self, title, author, isbn, cust, phone):
+            self.cur.execute("INSERT INTO sales VALUES (NULL,?,?,?,?,?)", (title, author, isbn, cust, phone,))
+            self.conn.commit()
 
     sdb = SDB()
 
@@ -171,7 +160,9 @@ def show_store_customer():      #Function to create window with customer functio
             list1.insert(END, row)
 
     def purchase_command():
-        print('Buy Books')
+        db.purchase(e1.get(), e2.get(), e4.get())
+        sdb.purchase(e1.get(), e2.get(), e3.get(), cust, phone)
+        messagebox.showinfo(title="Purchase", message="Purchase Successful!")
 
     window = Tk()
     window.title("Customer View")
@@ -183,9 +174,7 @@ def show_store_customer():      #Function to create window with customer functio
             window.destroy()
             del dd
 
-
     window.protocol("WM_DELETE_WINDOW", on_closing)
-
     l1 = Label(window, text="Title")
     l1.grid(row=0, column=0)
 
@@ -222,7 +211,6 @@ def show_store_customer():      #Function to create window with customer functio
 
     list1.configure(yscrollcommand=sb1.set)
     sb1.configure(command=list1.yview)
-
     list1.bind('<<ListboxSelect>>', get_selected_row)
 
     b1 = Button(window, text="View all", width=12, command=view_command)
@@ -237,8 +225,7 @@ def show_store_customer():      #Function to create window with customer functio
     b4 = Button(window, text="Close", width=12, command=window.destroy)
     b4.grid(row=5, column=3)
 
-
-def show_store_employee():       #Function to create window for employee functions and establish db connections
+def show_store_employee():  # Function to create window for employee functions and establish db connections
     class DB:
         def __init__(self):
             self.conn = sqlite3.connect("bookstore.db")
@@ -261,7 +248,8 @@ def show_store_employee():       #Function to create window for employee functio
             self.view()
 
         def update(self, id, title, author, isbn, quantity):
-            self.cur.execute("UPDATE book SET title=?, author=?, isbn=?, quantity=? WHERE id=?", (title, author, isbn, quantity, id,))
+            self.cur.execute("UPDATE book SET title=?, author=?, isbn=?, quantity=? WHERE id=?",
+                                 (title, author, isbn, quantity, id,))
             self.conn.commit()
             self.view()
 
@@ -282,7 +270,7 @@ def show_store_employee():       #Function to create window for employee functio
             self.conn = sqlite3.connect("sales.db")
             self.cur = self.conn.cursor()
             self.cur.execute(
-                "CREATE TABLE IF NOT EXISTS sales (orderID INTEGER PRIMARY KEY, customer TEXT, itemPurchased TEXT)")
+                    "CREATE TABLE IF NOT EXISTS sales (orderID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, isbn TEXT, cust TEXT, phone TEXT)")
             self.conn.commit()
 
         def __del__(self):
@@ -307,23 +295,17 @@ def show_store_employee():       #Function to create window for employee functio
             self.cur.execute(
                 "CREATE TABLE IF NOT EXISTS manufacturer (manufacturerID INTEGER PRIMARY KEY, title TEXT, author TEXT, isbn TEXT, quantity TEXT)")
             self.conn.commit()
-
         def __del__(self):
             self.conn.close()
-
         def view(self):
             self.cur.execute("SELECT * FROM manufacturer")
             rows = self.cur.fetchall()
             return rows
-
         def insert(self, title, author, isbn, quantity):
             self.cur.execute("INSERT INTO manufacturer VALUES (NULL,?,?,?,?)", (title, author, isbn, quantity,))
             self.conn.commit()
             self.view()
-
-
     mdb = MDB()
-
     def get_selected_row(event):
         global selected_tuple
         index = list1.curselection()[0]
@@ -336,12 +318,10 @@ def show_store_employee():       #Function to create window for employee functio
         e3.insert(END, selected_tuple[3])
         e4.delete(0, END)
         e4.insert(END, selected_tuple[4])
-
     def view_command():
         list1.delete(0, END)
         for row in db.view():
             list1.insert(END, row)
-
     def search_command():
         list1.delete(0, END)
         for row in db.search(e1.get(), e2.get()):
@@ -425,60 +405,43 @@ def show_store_employee():       #Function to create window for employee functio
         manufacturerWindow.title("Manufacturer Order")
         manufacturerWindow.iconbitmap('bythebooks.ico')
 
-
         l1 = Label(manufacturerWindow, text="Title")
         l1.grid(row=0, column=0)
-
         l2 = Label(manufacturerWindow, text="Author")
         l2.grid(row=0, column=2)
-
         l3 = Label(manufacturerWindow, text="ISBN")
         l3.grid(row=1, column=0)
-
         l4 = Label(manufacturerWindow, text="Quantity")
         l4.grid(row=1, column=2)
-
         title_text = StringVar()
         e1 = Entry(manufacturerWindow, textvariable=title_text)
         e1.grid(row=0, column=1)
-
         author_text = StringVar()
         e2 = Entry(manufacturerWindow, textvariable=author_text)
         e2.grid(row=0, column=3)
-
         isbn_text = StringVar()
         e3 = Entry(manufacturerWindow, textvariable=isbn_text)
         e3.grid(row=1, column=1)
-
         quantity_text = StringVar()
         e4 = Entry(manufacturerWindow, textvariable=quantity_text)
         e4.grid(row=1, column=3)
-
         list2 = Listbox(manufacturerWindow, height=25, width=65)
         list2.grid(row=2, column=0, rowspan=6, columnspan=2)
-
         sb2 = Scrollbar(manufacturerWindow)
         sb2.grid(row=2, column=2, rowspan=6)
-
         list2.configure(yscrollcommand=sb2.set)
         sb2.configure(command=list2.yview)
-
         list2.bind('<<ListboxSelect>>', get_selected_row)
-
         b1 = Button(manufacturerWindow, text="View orders", width=12, command=view_manufacturer)
         b1.grid(row=2, column=3)
-
         b3 = Button(manufacturerWindow, text="Add order", width=12, command=add_manufacturer)
         b3.grid(row=4, column=3)
-
         b5 = Button(manufacturerWindow, text="Delete selected", width=12, command=delete_manufacturer)
         b5.grid(row=6, column=3)
-
 
     window = Tk()
     window.title("Employee View")
     window.iconbitmap('bythebooks.ico')
-
 
     def on_closing():
         dd = db
@@ -521,10 +484,8 @@ def show_store_employee():       #Function to create window for employee functio
 
     sb1 = Scrollbar(window)
     sb1.grid(row=2, column=2, rowspan=6)
-
     list1.configure(yscrollcommand=sb1.set)
     sb1.configure(command=list1.yview)
-
     list1.bind('<<ListboxSelect>>', get_selected_row)
 
     b1 = Button(window, text="View all", width=12, command=view_command)
@@ -553,3 +514,4 @@ def show_store_employee():       #Function to create window for employee functio
 
 
 root.mainloop()
+
